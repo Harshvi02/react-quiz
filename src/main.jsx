dc39721a7004d1423f439b827074e45cfa0efac5
Harshvi02 <230493131002.harshvi@gmail.com>
2026-02-16 17:27:13 +0530

    Setup quiz UI and add QuizContext provider

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { QuizProvider } from "./contexts/QuizContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+  <QuizProvider>
     <App />
-  </React.StrictMode>
+  </QuizProvider>
+</React.StrictMode>
+
 );
