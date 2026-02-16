3cf446edcd4924af7ae86e2a3fa8a662ad68cc11
Harshvi02 <230493131002.harshvi@gmail.com>
2026-02-16 18:51:23 +0530

    Setup QuizContext with reducer and fetch questions from json-server

--- src/contexts/QuizContext.jsx
+++ src/contexts/QuizContext.jsx
@@ -1,10 +1,39 @@
-import { createContext } from "react";
+import { createContext, useReducer, useEffect } from "react";
 
 const QuizContext = createContext();
 
+const initialState = {
+  questions: [],
+  status: "loading",
+  index: 0,
+  answer: null,
+  points: 0,
+};
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+
+    default:
+      return state;
+  }
+}
+
 function QuizProvider({ children }) {
+  const [state, dispatch] = useReducer(reducer, initialState);
+ useEffect(function () {
+    fetch("http://localhost:9000/questions")
+      .then((res) => res.json())
+      .then((data) =>
+        dispatch({ type: "dataReceived", payload: data })
+      );
+  }, []);
   return (
-    <QuizContext.Provider value={{}}>
+    <QuizContext.Provider value={{ state, dispatch }}>
       {children}
     </QuizContext.Provider>
   );
